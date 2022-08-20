0d76c97a398c3eb55e0bd4cc58e02349c169bb80
0xblank <blank@debian>
2022-08-20 15:14:30 +0200

    Retrieve main caracteristics of nmap discovered ports

--- recon.py
+++ recon.py
@@ -8,4 +8,4 @@ def nmap_scan(ip: str) -> str:
     nmap_proc.run()
     open_ports = utils.parse_nmap_xml(nmap_proc.stdout)
     for port in open_ports:
-        print('[+] Detected port {} open'.format(port))
+        print('[+] Detected port {} {} : {} {} {} {}'.format(port['port_num'], port['state'], port['protocol'], port['service_type'], port['product_name'], port['product_version']))
--- utils.py
+++ utils.py
@@ -5,6 +5,15 @@ def parse_nmap_xml(target:str) -> str:
     doc = xml.dom.minidom.parseString(target)
     ports_xml = doc.getElementsByTagName('port')
     open_ports = []
-    for port_id in ports_xml:
-        open_ports.append(port_id.getAttribute('portid'))
+    for i, port_xml in enumerate(ports_xml):
+        port = {}
+        port['port_num'] = port_xml.getAttribute('portid')
+        port['protocol'] = port_xml.getAttribute('protocol')
+        state = doc.getElementsByTagName('state')[i]
+        port['state'] = state.getAttribute('state')
+        service = doc.getElementsByTagName('service')[i]
+        port['service_type'] = service.getAttribute('name')
+        port['product_name'] = service.getAttribute('product')
+        port['product_version'] = service.getAttribute('version')
+        open_ports.append(port)
     return(open_ports)
