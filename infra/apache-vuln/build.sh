8a2b8cf48e5d9577bb6430a463891ae4cb5fd44c
Steels <87060751+Steels03@users.noreply.github.com>
2022-09-15 14:25:42 +0200

    Update build.sh
    Modify local port

--- infra/apache-vuln/build.sh
+++ infra/apache-vuln/build.sh
@@ -1,2 +1,2 @@
 docker build -t apache-vuln .
-docker run --name apache-vuln -d -p 4444:80 apache-vuln httpd -D FOREGROUND
+docker run --name apache-vuln -d -p 4444:8080 apache-vuln httpd -D FOREGROUND
