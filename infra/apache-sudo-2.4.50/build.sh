02e2866ae8b8d34028240e77f35052cc71208471
0xblank <26724501+0xblank@users.noreply.github.com>
2022-10-13 21:04:28 +0200

    Apache-sudo-2.4.50 fix build.sh

--- infra/apache-sudo-2.4.50/build.sh
+++ infra/apache-sudo-2.4.50/build.sh
@@ -1,2 +1,2 @@
 docker build -t apache-sudo-2.4.50 .
-docker run --name apache-sudo-2.4.50 -d --ip 127.0.0.1 -p 4444:8080 apache-sudo httpd -D FOREGROUND
+docker run --name apache-sudo-2.4.50 -d --ip 127.0.0.1 -p 3333:8080 apache-sudo-2.4.50 httpd -D FOREGROUND
