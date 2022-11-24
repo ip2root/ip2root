97dc0fdc3237d0aa6e59f15dbab2d102d3d36eb1
steels <steels.github@gmail.com>
2022-11-24 16:48:22 +0100

    add c2 deploying script

---
+++ c2/build.sh
@@ -0,0 +1,6 @@
+docker run -it -d -p 1337:1337 -p 5000:5000 -p 8888:8888 --name empire bcsecurity/empire:latest
+curl -L https://github.com/BC-SECURITY/Starkiller/releases/download/v1.10.0/starkiller-1.10.0.AppImage -o starkiller
+chmod +x ./starkiller
+token=$(curl https://localhost:1337/api/admin/login -s --insecure -H "Content-Type: application/json" -X POST -d '{"username":"empireadmin", "password":"password123"}' | cut -d'"' -f4)
+curl --insecure -i -H "Content-Type: application/json" https://localhost:1337/api/listeners/http\?token\=$token -X POST -d '{"Name":"CLIHTTP", "Port":"8888"}'
+./starkiller &
